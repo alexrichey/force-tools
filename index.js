ee758f80089dd1972757c9b0afdebd42300547a9
Alex Richey <alex.richey@bluewolfgroup.com>
2017-01-14 14:24:31 -0500

    Various changes?

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-var express = require('express');
-var lightning = require('./resources/lightning.json');
+var express = require('express'),
+    setSymbolTable = require('./test.js'),
+    app = express(),
+    symbolTable = {},
+    lightning = require('./resources/lightning.json');
 
-var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+function getComponents () {
+  var components = lightning.components;
+  var keys = Object.keys(components);
+  var completions = [];
+  
+  for(var i = 0; i < keys.length; i++) {
+    var child = components[keys[i]];
+    if (typeof(child) === 'object') {
+      Object.keys(child).map(function (k) {
+        completions.push(keys[i] + ":" + k);
+      });
+    }
+  }
+  return completions;
+};
 
 app.use('/complete/me', function (req, res, next) {
-  console.log(lightning.ui);
-  res.send({"objects":[
-    {'Name': 'Tester'},
-    {'Name': 'Other'}]});
+  res.send(getComponents());
 });
 
-app.get('/completions', function (req, res) {
-  res.send(res);
+app.use('/gen-symboltable', function (req, res, next) {
+  setSymbolTable(res.send);
 });
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
+
+
+// sample symbol table query
+// /services/data/v37.0/tooling/query/?q=Select+Id,Name,SymbolTable+From+ApexClass
